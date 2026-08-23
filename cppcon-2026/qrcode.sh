a88c783e6af80ffade10251ae4c7e69213a1cd19
Kris Jusiak <kris@jusiak.net>
2026-08-23 02:07:16 +0100

    cppcon-2026

--- cppcon-2026/qrcode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qrencode -o images/qr.png "https://kris-jusiak.github.io/talks/cppcon-2026" -c -m1
---
+++ cppcon-2026/scripts/qrcode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+qrencode -o ../images/qr.png "https://kris-jusiak.github.io/talks/cppcon-2026" -c -m1
